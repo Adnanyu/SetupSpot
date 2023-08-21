a46e84cb8456805b803f5b81c896429b246fe7a3
adnan <adnanyu>
2023-08-22 01:55:42 +0300

    added auth and login custome hooks

---
+++ client-side/src/components/auth.component/useAuth.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useLogin } from "../../store/userSlice";
+import axios from "axios";
+
+export const useAuth = () => {
+  const dispatch = useDispatch()
+  const [isLoading, setIsLoading] = useState(false)
+  const [formData, setFormData] = useState({
+    name: '',
+    username: '',
+    password: '',
+    lastname: '',
+  });
+  const navigate = useNavigate()
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true)
+    try {
+      const response = await axios
+        .post(
+          'http://localhost:8000/users',
+          { ...formData },
+          { withCredentials: true }
+      )
+        dispatch(useLogin(response.data.user))
+        navigate('/posts')
+        alert(response.data.message)
+    } catch (err) {
+      alert(err.response.data.message);
+      setIsLoading(false)
+    }
+
+    // console.log(formData);
+  };
+    
+    return {
+        handleSubmit,
+        handleChange,
+        formData,
+        isLoading,
+        navigate
+    }
+}
---
+++ client-side/src/routes/login/useLogin.js
@@ -0,0 +1,49 @@
+import axios from 'axios';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useLogin } from '../../store/userSlice';
+
+export const useLoginHook = () => {
+  const dispatch = useDispatch()
+  const [isLoading, setIsLoading] = useState(false)
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+  });
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true)
+    try {
+      await axios
+        .post(
+          'http://localhost:8000/users/login',
+          { ...formData },
+          { withCredentials: true }
+        )
+        .then((res) => {
+          dispatch(useLogin(res.data.user))
+          alert(res.data.message)
+        });
+      navigate('/posts')
+    } catch (err) {
+      alert(err.response.data);
+      setIsLoading(false)
+    }
+
+    console.log(formData);
+  };
+    return {
+        handleSubmit,
+        handleChange,
+        formData,
+      isLoading,
+        navigate
+    }
+}
